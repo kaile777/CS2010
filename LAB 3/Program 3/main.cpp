09ba22cb4c2fe157afce645f8dd349402630eeab
Kaile Jones <kaile_jones_@outlook.com>
2024-09-25 17:28:58 -0600

    committing the main cpp file

---
+++ LAB 3/Program 3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    typedef int *VEC;
+    typedef VEC *MATRIX;
+
+    int a = 5;
+    VEC v = &a;
+    MATRIX m = &v;
+
+    cout << "Display a using v: " << *v << endl;
+    cout << "Display a using m: " << **m << endl;
+
+    return 0;
+    
+}
