f2e107c715bd593357c6ab95590ff8a3656e5367
Kaile Jones <kaile_jones_@outlook.com>
2024-09-25 16:55:58 -0600

    Commiting the main program file for project 1, along with the file containing global variables used in the main program.

---
+++ LAB 3/Program 1/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+extern const int given; 
+extern int cost, change;
+extern int quarters, dimes, nickels, pennies,
+    cost, change;
+
+int main() {
+
+    // User inputs the cost of the item to purchase
+    cout << "\nEnter a positive integer (<= 100): ";
+    cin >> cost;
+    cout << endl;
+
+    // Prints formatted total cost of item to console
+    cout << "COST: $";
+    (cost != 100) ? (cout << "0." << cost) : (cout << "1.00");
+    cout << endl;
+
+    change = given - cost;
+
+    // Prints formatted change to give
+    cout << "\nCHANGE: $0." << change << endl << endl;  
+
+    /*
+    Loop ensures that the least amount of total coins are used
+    by starting from quarters, and working way down to nickels.
+    */
+    while (change != 0) {
+        if ((change - 25) >= 0) {
+            change -= 25;
+            quarters++;
+        }
+        else if ((change - 10) >= 0) {
+            change -= 10;
+            dimes++;
+        }
+        else if ((change - 5) >= 0) {
+            change -= 5;
+            nickels++;
+        }
+        else if ((change - 1) >= 0) {
+            change -= 1;
+            pennies++;
+        }
+        else 
+            break;
+    }
+
+    // Summary of total coins given in each category as change.
+    cout << "Quarters : " << quarters << endl;
+    cout << "Dimes    : " << dimes << endl;
+    cout << "Nickels  : " << nickels << endl;
+    cout << "Pennies  : " << pennies << endl;
+
+}
---
+++ LAB 3/Program 1/vars.cpp
@@ -0,0 +1,16 @@
+
+// represents dollar amount used for payment in pennies
+int given = 100;
+
+// cost represents cost in pennies
+int cost;
+
+// change is the pennies result of (given - cost)
+int change;
+
+/*
+Variables for the types of coins represent the 
+amount of each coin given as change
+*/
+int quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+
